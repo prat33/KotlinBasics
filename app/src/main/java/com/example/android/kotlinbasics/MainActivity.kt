5d430eacc3b693ce4b09c36575fda3f1faf624f0
prat33 <pratheekn2001@gmail.com>
2020-11-28 16:46:30 +0530

    roll btn used

--- app/src/main/java/com/example/android/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlinbasics/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.android.kotlinbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var rollButton: Button = findViewById(R.id.roll_btn)
+        rollButton.setOnClickListener() {
+            var toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
